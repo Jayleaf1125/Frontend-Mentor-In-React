6fef7faad651f8f19ef7ef4a8e06db9fa2a3941f
Jason Padilla <84742412+Jayleaf1125@users.noreply.github.com>
2023-03-20 15:57:28 -0400

    Setup for website & results component completed

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+import Results_Summary_Component from './components/Results-Summary-Component/Results-Summary-Component'
+
+function App() {
+  return (
+    <div className="App">
+      <Results_Summary_Component />
+    </div>
+  )
+}
+
+export default App
