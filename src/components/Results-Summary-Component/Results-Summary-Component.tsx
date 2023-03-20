3b61168070ded5145e58a07da4fba95e821415d3
Jason Padilla <84742412+Jayleaf1125@users.noreply.github.com>
2023-03-20 17:35:41 -0400

    Completed Results Section

--- src/components/Results-Summary-Component/Results-Summary-Component.tsx
+++ src/components/Results-Summary-Component/Results-Summary-Component.tsx
@@ -1,23 +1,51 @@
-
 import styles from "./styles.module.css";
 
-export default function Results_Summary_Component()
-{
-    return (
-        <div className={styles.container}>
-        {/* Result Container */}
-        <div className={styles.resultsContainer}>
-            <h2>Your Result</h2>
-        </div>
-        {/* Summary Container */}
-        <div className={styles.summaryContainer}>
-            <h2>Summary</h2>
-            <h3>Reaction 80 / 100</h3>
-            <h3>Memory 92 / 100</h3>
-            <h3>Verbal 61 / 100</h3>
-            <h3>Visual 72 / 100</h3>
-            <button>Continue</button>
-        </div>
-        </div>
-    )
+export default function Results_Summary_Component() {
+  return (
+    <div className={styles.container}>
+      {/* Result Container */}
+      <div className={styles.resultsContainer}>
+        <h3>Your Result</h3>
+        <h1 className={styles.ellipse}>
+          {" "}
+          76 <span className={styles.numbersInResult}> of 100</span>
+        </h1>
+        <h2>Great</h2>
+        <p className={styles.resultsBio}>
+          You scored higher than 65% of the people who have taken these tests.
+        </p>
+      </div>
+      {/* Summary Container */}
+      <div className={styles.summaryContainer}>
+        <h2>Summary</h2>
+        <ul>
+          <li>
+            <span className={styles.textOne}>Reaction</span>
+            <span className={styles.numbers}>
+              <b>80</b> / 100
+            </span>
+          </li>
+          <li>
+            <span className={styles.textTwo}>Memory</span>{" "}
+            <span className={styles.numbers}>
+              <b>92</b> / 100
+            </span>
+          </li>
+          <li>
+            <span className={styles.textThree}>Verbal</span>
+            <span className={styles.numbers}>
+              <b>61</b> / 100
+            </span>
+          </li>
+          <li>
+            <span className={styles.textFour}>Visual</span>
+            <span className={styles.numbers}>
+              <b>72</b> / 100
+            </span>
+          </li>
+          <button>Continue</button>
+        </ul>
+      </div>
+    </div>
+  );
 }
